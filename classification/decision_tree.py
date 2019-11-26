939cc4bbd4407470b4f73afdf322c12ff03d00f6
Jairo Souza <pto.raphael@gmail.com>
2019-11-26 20:31:51 -0300

    Glass dataset

--- classification/decision_tree.py
+++ classification/decision_tree.py
@@ -5,19 +5,16 @@ Created on Sat Nov  2 22:04:07 2019
 """
 
 # Importando os pacotes
-from __future__ import absolute_import
-from utils import plot_results_class, accuracy, f_measure, feature_scaling
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, recall_score, accuracy_score, precision_score
+from sklearn.model_selection import cross_validate
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
 
 # Importando os dados
-df = pd.read_csv('data/titanic.csv')
-
-# Selecionando uma amostragem dos dados para melhor visualização
-df = df.sample(n=100, random_state=0)
+df = pd.read_csv('data/glass.csv')
 
 # Descrevendo o dataset
 df.describe()
@@ -25,40 +22,57 @@ df.describe()
 # Visualizando o dataset
 df.head(5)
 
-# Preenchendo os valores númericos nulos (NA) com a mediana.
-df = df.fillna(df.median())
-
 # Definindo as variáveis dependentes/independentes.
-X = df.iloc[:, [5, 9]].values
-y = df.iloc[:, 1].values
+X = df.iloc[:, :10].values
+y = df.iloc[:, -1].values
 
-# Criando os subconjuntos de treinamento e testes
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Normalizando as features 
-X_train = feature_scaling(X_train)
-X_test = feature_scaling(X_test)
 
-# Treinando o modelo de Árvore de Decisão com o Conjunto de Treinamento
+# 1ª Maneira de trreinamento e avaliação
+#Definindo as métricas a serem utilizadas:
+metrics = ['accuracy', 'precision_macro', 'recall_macro', 'f1_macro']
+
+# Treinando o modelo de Árvore de Decisão usando a validação cruzada com 10 folds:
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, y_train)
 
-# Prevendo os resultados do modelo criado com o conjunto de testes
-y_pred = classifier.predict(X_test)
+scores = cross_validate(classifier, X, y, cv=10, scoring=metrics)
 
-# Criando a matriz de confusão com o conjunto de testes 
-tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+# Visualizando as métricas
+pd.DataFrame.from_dict(scores)
 
-# Visualizando a métrica de acurácia através das funções criandas e da bibilioteca sklearn
-accuracy(tp, fp, fn, tn)
-classifier.score(X_test, y_test)
+pd.DataFrame.from_dict(scores).mean()
 
-# Exibindo o f-measure
-f_measure(tp, fp, fn)
-f1_score(y_test, y_pred)  
 
-# Exibindo os resultados do conjunto de treinamento
-plot_results_class(X_train, y_train, classifier, 'Arvore de Decisão (Conj. de Treinamento)')
+# 2ª Maneira de trreinamento e avaliação
+# Defininndo o numero de folds: 5. Lembrando que também iremos realizar um "shuffle" nos dados:
+kf = KFold(n_splits=5, shuffle= True)
 
-# Exibindo os resultados do conjunto de testes
-plot_results_class(X_test, y_test, classifier, 'Arvore de Decisão (Conj. de Testes)')
+#Define o modelo a ser treinado 
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+
+# Criando dataframe que irá guardar os resultados finais das métricas
+df_results = pd.DataFrame(columns=['itera', 'acc', 'prec', 'rec', 'f1'], index=None)
+
+#Realizando o treinamento do classificador usando validação cruzada:
+
+itera = 0
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+ 
+    # Treina o modelo a cada iteração com o conjunto de treinamento:
+    classifier.fit(X_train, y_train)
+
+    # Prevendo os resultados do modelo criado com o conjunto de testes
+    y_pred = classifier.predict(X_test)
+       
+    # Armazenando os valores das métricas a cada iteração com o conjunto de teste em um df
+    itera += 1
+    df_results.loc[len(df_results), :] = [itera, accuracy_score(y_test, y_pred), precision_score (y_test, y_pred, average = 'macro'),
+                   recall_score(y_test, y_pred,  average = 'macro'), f1_score(y_test, y_pred,  average = 'macro')]
+    
+#Criando um dataframe contendoo valor das métricas a cada iteração:
+df_results
+
+#Exibindo a media das metricas:
+df_results.mean()
---
+++ complementar/decision_tree.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 19:27:10 2019
+
+@author: Jairo Souza
+"""
+
+# Importando os pacotes
+from __future__ import absolute_import
+from utils import plot_results_class, accuracy, f_measure, feature_scaling
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+
+# Importando os dados
+df = pd.read_csv('data/titanic.csv')
+
+# Selecionando uma amostragem dos dados para melhor visualização
+df = df.sample(n=100, random_state=0)
+
+# Descrevendo o dataset
+df.describe()
+
+# Visualizando o dataset
+df.head(5)
+
+# Preenchendo os valores númericos nulos (NA) com a mediana.
+df = df.fillna(df.median())
+
+# Definindo as variáveis dependentes/independentes.
+X = df.iloc[:, [5, 9]].values
+y = df.iloc[:, 1].values
+
+# Criando os subconjuntos de treinamento e testes
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Normalizando as features 
+X_train = feature_scaling(X_train)
+X_test = feature_scaling(X_test)
+
+# Treinando o modelo de Árvore de Decisão com o Conjunto de Treinamento
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Prevendo os resultados do modelo criado com o conjunto de testes
+y_pred = classifier.predict(X_test)
+
+# Criando a matriz de confusão com o conjunto de testes 
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+
+# Visualizando a métrica de acurácia através das funções criandas e da bibilioteca sklearn
+accuracy(tp, fp, fn, tn)
+classifier.score(X_test, y_test)
+
+# Exibindo o f-measure
+f_measure(tp, fp, fn)
+f1_score(y_test, y_pred)  
+
+# Exibindo os resultados do conjunto de treinamento
+plot_results_class(X_train, y_train, classifier, 'Arvore de Decisão (Conj. de Treinamento)')
+
+# Exibindo os resultados do conjunto de testes
+plot_results_class(X_test, y_test, classifier, 'Arvore de Decisão (Conj. de Testes)')
