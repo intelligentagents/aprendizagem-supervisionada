f6eff110c404c3f6a31c0d41bd6d144eb51286b1
Jairo Souza <pto.raphael@gmail.com>
2019-12-04 04:13:43 -0300

    Adding Docker Composer to build the React and Flask webapp

--- ml-apps/iris-classifier-flask-react/service/app.py
+++ ml-apps/iris-classifier-flask-react/service/app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify, make_response
 from flask_restplus import Api, Resource, fields
 from sklearn.externals import joblib
 import numpy as np
-import sys
 
 flask_app = Flask(__name__)
+
 app = Api(app = flask_app, 
 		  version = "1.0", 
 		  title = "Iris Plant identifier", 
